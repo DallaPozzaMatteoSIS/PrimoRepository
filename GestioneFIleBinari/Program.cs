aedba6f88b3eab0360c0e9717e29ea7a9d33836f
DallaPozzaMatteoSIS <matteo.dallapozza@issgreppi.it>
2025-05-27 09:43:12 +0200

    s

---
+++ GestioneFIleBinari/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace GestioneFIleBinari;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string baseFolder = AppDomain.CurrentDomain.BaseDirectory;
+        string fileName = "dati.txt";
+        string path = Path.Combine(baseFolder, fileName);
+        FileStream stream = new(path, FileMode.Open);
+        using (stream)
+        {
+            stream.Seek(0, SeekOrigin.Begin);
+        }
+        Console.ReadKey();
+    }
+}
